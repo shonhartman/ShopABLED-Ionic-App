cf48addd6dcbc9a63e152a9acef56cec79d6508c
Shaun Hartman <shaunhartman@Shauns-MacBook-Pro.local>
2016-08-02 00:45:05 -0500

    styling

--- app/modules/chats/controllers/list.js
+++ app/modules/chats/controllers/list.js
@@ -2,7 +2,34 @@ class ChatsController {
   constructor(ChatsService, $scope) {
     this._ChatsService = ChatsService;
     this.chats = ChatsService.all();
+    $scope.products = ["Eggs", "Milk", "Cake","Bread","butter"];
+    $scope.addItem = function() {
+    $scope.addMe= $scope.addMe.charAt(0).toUpperCase() + $scope.addMe.slice(1).toLowerCase();
+    //return $scope.addMe;
+    $scope.errortext = "";
+    if (!$scope.addMe) {
+      return;
+    }
+  if ($scope.products.indexOf($scope.addMe) == -1) {
+    $scope.products.push($scope.addMe);
+  } else {
+    $scope.errortext = "* The item is already in your grocery list.";
+    $scope.removeItem = function(x) {
+   $scope.errortext = "";
+   $scope.products.splice(x, 1);
+ }
   }
+}
+
+function mover(x) {
+  x.style.backgroundColor = " #ff9933";
+
+}
+function mout(x) {
+ x.style.backgroundColor ="#FFFFFF" ;
+}
+  }
+
 
   remove(chat) {
     this._ChatsService.remove(chat);
