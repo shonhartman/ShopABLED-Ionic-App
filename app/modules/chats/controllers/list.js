02731ff775d64d506d8467b6c08dcf4ae37bb171
Shaun Hartman <shaunhartman@Shauns-MacBook-Pro.local>
2016-08-09 00:13:56 -0500

    Simple Input List Working!

--- app/modules/chats/controllers/list.js
+++ app/modules/chats/controllers/list.js
@@ -1,6 +1,29 @@
+
 class ChatsController {
-  constructor($ionicModal, $scope) {
-    $scope.tasks = [];
+  constructor($scope) {
+    $scope.todos = [
+      {'title':'Build a todo app', 'done':false},
+    ];
+    $scope.addTodo = function(){
+      console.log('WTF');
+      $scope.todos.push({'title':this.newTodo,'done':false})
+      this.newTodo = '';
+    }
+    $scope.clearCompleted = function(){}
+  }
+}
+
+
+
+
+
+
+
+
+//////////WORKING///////////////////////////////////////////////////
+// class ChatsController {
+  // constructor($ionicModal, $scope) {
+    // $scope.tasks = [];
  // [  { title: 'Carrots' },
  //   { title: 'Celery' },
  //   { title: 'Spinach' },
@@ -8,20 +31,20 @@ class ChatsController {
  // ];
 
     // Called when the form is submitted
-    $scope.createTask = function(task) {
-      $scope.tasks.push({
-        title: task.title
-      });
-      task.title = "";
-      $scope.createItemField.$setPristine();
-      alert("pristine");
-    };
+    // $scope.createTask = function(task) {
+    //   $scope.tasks.push({
+    //     title: task.title
+    //   });
+    //   task.title = "";
+    //   $scope.createItemField.reset();
+    // };
 
     // Open our new task modal
     // $scope.newTask = function() {
     //   $scope.taskModal.show();
     // };
-  }
-}
+//   }
+// }
+/////////////////////////////////////////////////////////////////////////
 
 export default ChatsController;
