ba2061b90ce2b5bc5c250b0bf664085117af984a
Shaun Hartman <shaunhartman@Shauns-MacBook-Pro.local>
2016-08-10 00:11:09 -0500

    List View looking good

--- app/modules/chats/controllers/list.js
+++ app/modules/chats/controllers/list.js
@@ -13,38 +13,4 @@ class ChatsController {
   }
 }
 
-
-
-
-
-
-
-
-//////////WORKING///////////////////////////////////////////////////
-// class ChatsController {
-  // constructor($ionicModal, $scope) {
-    // $scope.tasks = [];
- // [  { title: 'Carrots' },
- //   { title: 'Celery' },
- //   { title: 'Spinach' },
- //   { title: 'Beer' }
- // ];
-
-    // Called when the form is submitted
-    // $scope.createTask = function(task) {
-    //   $scope.tasks.push({
-    //     title: task.title
-    //   });
-    //   task.title = "";
-    //   $scope.createItemField.reset();
-    // };
-
-    // Open our new task modal
-    // $scope.newTask = function() {
-    //   $scope.taskModal.show();
-    // };
-//   }
-// }
-/////////////////////////////////////////////////////////////////////////
-
 export default ChatsController;
