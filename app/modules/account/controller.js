69757ebe6ab9a6019693f3ab75c968efbdcc1a44
Shaun Hartman <shaunhartman@Shauns-MacBook-Pro.local>
2016-08-18 01:07:22 -0500

    ..still going for list population

--- app/modules/account/controller.js
+++ app/modules/account/controller.js
@@ -1,47 +1,18 @@
 class AccountController {
   constructor($scope) {
-    $scope.todos = [];
-
-    $scope.clearCompleted = function(){
-      $scope.todos = $scope.todos.filter(function(item) {
-        return !item.done
-      })
-
-      // Initialization
-      $scope.onEditChangeResult = "";
-
-      // Event handlers
-      $scope.onEditChange = function () {
-        $scope.onEditChangeResult = "the value is '" + $scope.newTodo + "'";
-      };
-    }
-
-///ngRepeat Todo List Example
     $scope.groceryList = [];
 
-    $scope.itemAdd = function() {
-      $scope.groceryList.push({itemText:$scope.itemInput, done:false});
-        $scope.itemInput = "";
-    };
-
-    $scope.remove = function() {
-      var oldList = $scope.groceryList;
-      $scope.groceryList = [];
-      angular.forEach(oldList, function(x) {
-        if (!x.done) $scope.groceryList.push(x);
-      });
-    };
-
-  // Let's define a command.
-  // This one populates to text field but not the list
-      var commands = {
-        'new item *val' : function(val) {
-          $scope.newTodo = val;
-          $scope.addTodo = function(){
-          }
-          $scope.$apply();
-         }
-      }
+    var commands = {
+      'new item *val' : function(val) {
+        $scope.itemInput = val;
+        $scope.itemAdd = function() {
+          $scope.groceryList.push({itemText:$scope.itemInput, done:false});
+            $scope.itemInput = "";
+            console.log($scope.groceryList[0].itemText);
+        };
+        $scope.$apply();
+       }
+    }
 
       // Add our commands to annyang
       annyang.addCommands(commands);
@@ -51,9 +22,16 @@ class AccountController {
       // Start listening.
       annyang.start();
 
+      $scope.remove = function() {
+      var oldList = $scope.groceryList;
+      $scope.groceryList = [];
+      angular.forEach(oldList, function(x) {
+          if (!x.done) $scope.groceryList.push(x);
+      });
+    };
   }
-
 }
+
 angular.module('ToDo',[]);
 export default AccountController;
 
