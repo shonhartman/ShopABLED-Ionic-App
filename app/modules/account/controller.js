f5c23edb7fec7ccfb36ffb7e2bfeb906683976b0
Shaun Hartman <shaunhartman@Shauns-MacBook-Pro.local>
2016-08-19 00:37:07 -0500

    ...working on remove item function.

--- app/modules/account/controller.js
+++ app/modules/account/controller.js
@@ -3,16 +3,30 @@ class AccountController {
     $scope.groceryList = [];
     $scope.itemAdd = function() {
       $scope.groceryList.push({itemText:$scope.itemInput, done:false});
-        // $scope.itemInput = "";
+        $scope.itemInput = "";
         console.log($scope.groceryList[0].itemText);
     };
 
+    $scope.remove = function() {
+    var oldList = $scope.groceryList;
+    $scope.groceryList = [];
+    angular.forEach(oldList, function(x) {
+        if (!x.done) $scope.groceryList.push(x);
+    });
+  };
 
     var commands = {
       'new item *val' : function(val) {
         $scope.itemInput = val;
         $scope.itemAdd();
         $scope.$apply();
+    },
+    'remove item *val' : function(val){
+      if($scope.itemInput == val) {
+        console.log("removed")
+        $scope.remove();
+        $scope.$apply();
+      }
     }
   }
 
@@ -23,19 +37,8 @@ class AccountController {
       annyang.addCommands(commands);
       // Start listening.
       annyang.start();
-
-      $scope.remove = function() {
-      var oldList = $scope.groceryList;
-      $scope.groceryList = [];
-      angular.forEach(oldList, function(x) {
-          if (!x.done) $scope.groceryList.push(x);
-      });
-    };
   }
 }
 
-
 angular.module('ToDo',[]);
 export default AccountController;
-
-//////////////////////////////////////////////
